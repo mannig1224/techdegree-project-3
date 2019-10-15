0dedfe1bf06d49569686d4368210738ffcd6fe47
manniig24 <egatica51@gmail.com>
2019-10-15 06:29:14 -0600

    added comments

--- interactive-form-v1/js/script.js
+++ interactive-form-v1/js/script.js
@@ -1,9 +1,15 @@
+
+// Places focus on the name input
 $(' #name ').focus();
 
+// We want to initially hide the input form for the "other" job-role
 $('#other-title').hide();
 
+
+// Listens to the job-role selection for any changes
 $('#title').on('change', function(event){
     const selectedRole = event.target.value;
+    // If 'other' is selected than we show the input field for other
     if (selectedRole === "other"){
         $('#other-title').show();
      } else {
@@ -12,18 +18,27 @@ $('#title').on('change', function(event){
 
 });
 
+
+// We want to initially hide the options to choose the shirt colors
 $('#color option').hide();
+
+// Dynamically adds another option that ask for the user to pick a theme
 $('#color').prepend('<option>"Please select a T-shirt theme."</option>');
 $('#color option').eq(0).attr("selected",true);
+
+
 
 $('#design').on('change', function(event){
     const selectedDesign = event.target.value;
     if (selectedDesign === "js puns"){
         $('#color option').eq(1).show().attr("selected",true).next().show().next().show();
         $('#color option').eq(4).hide().next().hide().next().hide();
+        $('#color option').eq(0).hide();
      } else {
         $('#color option').eq(4).show().attr("selected",true).next().show().next().show();
         $('#color option').eq(1).hide().next().hide().next().hide();
+        $('#color option').eq(0).hide();
+
      }
 
 });
